9f51510841ff22ac4de82e164b1a5e3d965aef01
Devesh Shetty <devesh.shetty007@gmail.com>
2016-05-15 04:20:12 +0530

    Updated comments on BFS

---
+++ structures/BFS.java
@@ -0,0 +1,13 @@
+package structures;
+
+/*
+*<h1>Breadth First Traversal For a Graph</h1>
+*<p>
+*Breadth-first search is a way to find all the vertices reachable from the a given source vertex. 
+*the basic idea of the breath-first search is this: send a wave out from source s. The wave hits all vertices 1 edge from s. From there, the wave hits all vertices 2 edges from s and so on.
+*@author: Devesh Shetty
+*/
+class BFS{
+    
+    
+}
