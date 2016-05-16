4d792d5ff51d1da1e6232d799487bcbb641584ad
Devesh Shetty <devesh.shetty007@gmail.com>
2016-05-16 23:31:35 +0530

    Added one application of BFS

--- structures/BFS.java
+++ structures/BFS.java
@@ -5,6 +5,7 @@ package structures;
 *<p>
 *Breadth-first search is a way to find all the vertices reachable from the a given source vertex. 
 *the basic idea of the breath-first search is this: send a wave out from source s. The wave hits all vertices 1 edge from s. From there, the wave hits all vertices 2 edges from s and so on.
+*In Peer to Peer Networks like BitTorrent, Breadth First Search is used to find all neighbor nodes.
 *@author: Devesh Shetty
 */
 class BFS{
