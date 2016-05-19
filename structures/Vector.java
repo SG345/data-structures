4483eed1abceca1397a759f2c020b79cf10ecaa3
Devesh Shetty <devesh.shetty007@gmail.com>
2016-05-19 13:35:27 +0530

    Implementing Vector

---
+++ structures/Vector.java
@@ -0,0 +1,21 @@
+/**
+*<h1>Vector</h1>
+*<p>
+*Vector containers are implemented as dynamic arrays; Just as regular arrays, vector containers have their elements stored in contiguous storage locations, which means that their elements can be accessed not only using iterators but also using offsets on regular pointers to elements.
+*But unlike regular arrays, storage in vectors is handled automatically, allowing it to be expanded and contracted as needed.
+*Vectors can hold any object. They still allow access in constant time to individual elements via index, just like an array.
+*
+*@author: Devesh Shetty
+*/
+public class Vector
+{
+    //the data
+    private Object data[];
+    //the element count
+    private int elementCount;
+    
+    public Vector(){
+        
+    }
+    
+}
