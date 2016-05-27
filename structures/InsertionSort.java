90f1a7fdfeefbb93cdd3a3a85dbb8c00dcde156c
Devesh Shetty <devesh.shetty007@gmail.com>
2016-05-27 20:13:01 +0530

    Implemented method sort using InsertionSort algorithm

---
+++ structures/InsertionSort.java
@@ -0,0 +1,36 @@
+package structures;
+
+
+/**
+*Insertion Sort
+*Sorts the array in place
+*
+*Best case: O(n); array already sorted
+*Worst case: O(n^2); array contains elements in non-decreasing order
+*@author: Devesh Shetty
+*/
+public class InsertionSort{
+    
+    /**
+    *Sorts the array in non-decreasing order
+    *Elements in the array who need to be sorted are known as key
+    *@param arr the array to be sorted in non-decreasing order
+    */
+    public static sort(int arr[]){
+        for(int i = 1; i < arr.length; i++){
+            // elements in the array to the left of i value are considered to be in sorted order
+            //key contains the element we need to place in the left of the array such elements < key are smaller than it
+            int key = arr[i];
+            int j = i - 1;
+            while( j >= 0 & arr[j] > key){
+                //shift the elememts one position to the right
+                arr[j + 1] = arr[j];
+                j-=1;
+            }
+            //place the key value in it's position
+            arr[j + 1] = key;
+            
+        }
+    }
+    
+}
