61941fa5bc52bb851e6ed1e40daf8e84d095f7d6
Devesh Shetty <devesh.shetty007@gmail.com>
2016-05-27 23:56:02 +0530

    Changed to shorthand & operation to prevent ArrayIndexOutOfBoundsException

--- structures/InsertionSort.java
+++ structures/InsertionSort.java
@@ -22,10 +22,10 @@ public class InsertionSort{
             //key contains the element we need to place in the left of the array such elements < key are smaller than it
             int key = arr[i];
             int j = i - 1;
-            while( j >= 0 & arr[j] > key){
+            while( j >= 0 && arr[j] > key){
                 //shift the elememts one position to the right
                 arr[j + 1] = arr[j];
-                j-=1;
+                j--;
             }
             //place the key value in it's position
             arr[j + 1] = key;
