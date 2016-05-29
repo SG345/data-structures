178421152d9cefbf3784b4d26f8149f7b9a973bc
Devesh Shetty <devesh.shetty007@gmail.com>
2016-05-29 13:21:09 +0530

    Implementing maximum sub-array

---
+++ structures/MaximumSubArray.java
@@ -0,0 +1,18 @@
+package structures;
+
+/**
+*The maximum sub-array is the problem in which we aim to get the sub-array which has the maximum sum
+*The elements of the sub-array should be a mix of negative as well as positive numbers otherwise a set of positive numbers would yield the entire array as the maximum sub-array
+*It uses the divide and conquer approach
+*The time complexity is O(nlg n); where lg n is the log of n to base 2
+*
+*@author: Devesh Shetty
+*/
+public MaximumSubArray{
+    
+    public static void findMaximumSubArray(){
+        
+    }
+    
+    
+}
