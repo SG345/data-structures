e19cd24931411dc484a1f92b777823eef68145c2
Devesh Shetty <devesh.shetty007@gmail.com>
2016-05-30 21:05:50 +0530

    Work in progress for heapsort algo

--- structures/HeapSort.java
+++ structures/HeapSort.java
@@ -2,19 +2,15 @@ package structures;
 
 /**
 *HeapSort
-*
+*It makes of the heap data structure; the heap data structure is an array object that can be viewed as a complete binary tree
 *The time complexity of HeapSort algorithm is O(nlg n); where lg n is log of n to base 2
 *
 *@author: Devesh Shetty
 */
 public class HeapSort{
     
-    public static void sort(){
-            
-    }
-    
     /**
-    *This method returns the index of the parent to the element at index i
+    *A helper method that returns the index of the parent to the element at index i
     *@param i the index of the child whose parent index needs to be found
     *@return parent index position
     */
@@ -24,7 +20,7 @@ public class HeapSort{
     } 
     
     /**
-    This method returns the index of the left child to the element at index i
+    *A helper method that returns the index of the left child to the element at index i
     *@param i the index whose left child index needs to be found
     *@return the left child index position
     */
@@ -33,12 +29,33 @@ public class HeapSort{
     }
     
     /**
-    *This method returns the index of the right child to parent at position i
+    *A helper method that returns the index of the right child to parent at position i
     *@param i the index whose right child needs to be found
     *@return int the index of the right child
     */
     private static int getRightChild(int i){
         return i << 2;
     }
+    
+    /**
+    *This method makes use of the HeapSort algorithm to sort the integer array in  non-decreasing fashion
+    *@param arr the integer array
+    */
+    public static void sort(int arr[]){
+        
+    }
+    
+    /**
+    *This method builds the max heap
+    *@param arr[] the integer array
+    */
+    private static void buildMaxHeap(int arr[]){
+        // the elements in the subarray A[ (n/2 + 1) ... n] are all leaves of the tree
+        for( int i = (arr.length - 1)/2 ; i >= 0; i-- ){
+            maxHeapify(arr, i);
+        }
+    }
+    
+    
     
 }
