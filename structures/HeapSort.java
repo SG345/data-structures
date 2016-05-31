df1ac5180fea90fe34d54f3bfbf4370fbda0107e
Devesh Shetty <devesh.shetty007@gmail.com>
2016-05-31 18:14:09 +0530

    Added maxHeapify method to maintain max-heap property

--- structures/HeapSort.java
+++ structures/HeapSort.java
@@ -50,12 +50,52 @@ public class HeapSort{
     *@param arr[] the integer array
     */
     private static void buildMaxHeap(int arr[]){
+        //size of the heap
+        int heapSize = arr.length - 1;
         // the elements in the subarray A[ (n/2 + 1) ... n] are all leaves of the tree
         for( int i = (arr.length - 1)/2 ; i >= 0; i-- ){
-            maxHeapify(arr, i);
+            maxHeapify(arr, i, heapSize);
         }
     }
     
-    
+    /**
+    *This method lets the value at index i float down in the max-heap when binary trees rooted at getLeftChild(i) and getRightChild(i) are max-heaps, but that the element at index i may be smaller than it's children
+    *
+    *In max-heap, the max-heap property for every node other than root is A[getParent(i)] >= A[i]
+    *
+    *@param arr the integer array
+    *@param i the index
+    *@param heapSize the size of the heap
+    */
+    private static void maxHeapify(int arr[], int i, int heapSize){
+        
+        int leftChildIndex = getLeftChild(i),//the left child index
+            rightChildIndex = getRightChild(i);//the right child index
+        
+        //assume largest to be the element at index i
+        int largest = i;//stores the index value of the largest of three: i, leftChild(i), rightChild(i)
+        
+        if(leftChildIndex <= heapSize && arr[leftChildIndex] > arr[i]){
+            //left child has a greater value than i
+            largest = leftChildIndex;
+        }
+        
+        if(rightChildIndex <= heapSize && arr[rightChildIndex] > arr[largest] ){
+            //right child has a greater value
+            largest = rightChildIndex;
+        }
+        
+        if( largest != i){
+            //indicates one of the child has a greater value than it's parent(i.e. arr[i])
+            //exchange the element stored at arr[i] with the a[largest]
+            int temp = arr[largest];
+            arr[largest] = arr[i];
+            arr[i] = temp;
+            
+            maxHeapify(arr, largest, heapSize);
+        }
+        
+        
+    }
     
 }
