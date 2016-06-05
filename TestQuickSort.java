f015cc96e82bbfcc1ed53adf8465d7c63b82d22a
Devesh Shetty <devesh.shetty007@gmail.com>
2016-06-05 17:48:30 +0530

    Test cases for quicksort

---
+++ TestQuickSort.java
@@ -0,0 +1,28 @@
+import structures.QuickSort;
+import java.io.*;
+import java.util.Arrays;
+
+class Main{
+    
+    public static void main(String []args){
+        
+        int t1[] = {1,2,3,4,5,6};
+        int t2[] = {900,800,700,300,100,10,5,1};
+        int t3[] ={5,6,1,200,98,12,34};
+        printCustom(t1);
+        printCustom(t2);
+        printCustom(t3);
+    }
+    
+    
+    public static void printCustom(int t[]){
+        PrintWriter out = new PrintWriter(System.out,true);
+        out.println("Before sorting");
+        out.println(Arrays.toString(t));
+        MergeSort.sort(t);
+        out.println("After sorting");
+        out.println(Arrays.toString(t));
+        
+    }
+    
+}
