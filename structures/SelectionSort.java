b87e2f5efc7d75ff7f40f3b164844a5a5a86c86a
Devesh Shetty <devesh.shetty007@gmail.com>
2016-06-08 18:39:27 +0530

    Impelementing selection sort

---
+++ structures/SelectionSort.java
@@ -0,0 +1,20 @@
+package structures;
+
+/**
+*SelectionSort
+*
+*On every iteration and it picks the minimum out of the remaining and puts it in it's correct position
+*
+*@author: Devesh Shetty
+*/
+public class SelectionSort{
+    
+   
+    /**
+    *@param arr the integer array to be sorted
+    */
+    public static void sort(int arr[]){
+        
+    }
+    
+}
