477bcadd1b29fde289f45cd50b1cbaacc1c66212
Devesh Shetty <devesh.shetty007@gmail.com>
2016-06-08 18:49:42 +0530

    Implemented SelectionSort

--- structures/SelectionSort.java
+++ structures/SelectionSort.java
@@ -14,6 +14,25 @@ public class SelectionSort{
     *@param arr the integer array to be sorted
     */
     public static void sort(int arr[]){
+       
+        for(int i = 0; i< arr.length;i++){
+            
+            //the index of minimum value
+            int minIndex = i;
+            
+            for(int j = i+1;j< arr.length;j++){
+                //find the minimum in the array of length (arr.length - i)
+                if( arr[j] < arr[minIndex]){
+                    minIndex = j;
+                }
+            }
+            //swap the value of minimum with i index
+            int temp = arr[minIndex];
+            arr[minIndex] = arr[i];
+            arr[i] = temp;
+            
+            //on every iteration, the minimum value is placed at the ith position
+        }
         
     }
     
