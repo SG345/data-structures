bc20c69eadcccc411eef00eecdb897334efc79f3
Devesh Shetty <devesh.shetty007@gmail.com>
2016-07-05 00:10:46 +0530

    Working on Binary Trees

---
+++ structures/BinaryTree.java
@@ -0,0 +1,29 @@
+package structures;
+
+/**
+*<h1>Binary Tree </h1>
+*Trees are hierarchical data structures
+*A tree whose elements have at most 2 children is called binary tree.
+*Main uses of tree include maintaining hierarchical data, providing moderate access and insert/delete operations
+*@author: Devesh Shetty
+*/
+public class BinaryTree{
+    
+    private class Node{
+        int data;
+        Node leftChild;
+        Node rightChild;
+    }
+    
+    public void insert(int data){
+        
+    }
+    
+    public void delete(int data){
+        
+    }
+    
+    public 
+    
+    
+}
