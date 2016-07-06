dbb7108930b0c6329725c71c2ee005497d6e25eb
Devesh Shetty <devesh.shetty007@gmail.com>
2016-07-06 13:44:40 +0530

    Added the relevant methods and their documentation

--- structures/BinaryTree.java
+++ structures/BinaryTree.java
@@ -1,5 +1,5 @@
 package structures;
-
+import java.util.Iterator;
 /**
 *<h1>Binary Tree </h1>
 *Trees are hierarchical data structures
@@ -8,6 +8,7 @@ package structures;
 *@author: Devesh Shetty
 */
 public class BinaryTree<E>{
+    
     //the value of the node
     protected E val;
     //the parent of the node
@@ -16,26 +17,121 @@ public class BinaryTree<E>{
     protected BinaryTree<E> left, right;
     
     /**
-    *
+    *Constructor that generates an empty node
     */
-    public BinaryTree<E>(){
+    public BinaryTree(){
         
     }
     
     /**
-    *
+    *Constructor that generates a tree referencing value and two empty sub-trees
+    *@param value; the value of the node
     */
-    public BinaryTree<E>(E value){
+    public BinaryTree(E value){
         
     }
     
     /**
-    *
+    *Constructor that generates a tree referencing the value and two subtrees
+    *@param value; the value of the node
+    *@param leftsubTree the left sub-tree of the node
+    *@param rightsubTree the right sub-tree of the node
     */
-    public BinaryTree<E>(E value, BinaryTree<E> leftsubTree, BinaryTree<E> rightsubTree){
+    public BinaryTree(E value, BinaryTree<E> leftsubTree, BinaryTree<E> rightsubTree){
         
     }
     
+    /**
+    *This method returns a reference to left subtree
+    *@return left Binary subtree
+    */
+    public BinaryTree<E> left(){
+        
+    }
+    
+    /**
+    *This method returns a reference to right subtree
+    *@return right Binary subtree
+    */
+    public BinaryTree<E> right(){
+        
+    }
+    
+    /**
+    *This method returns a reference to the parent node or null
+    *@return the parent if exists else null 
+    */
+    public BinaryTree<E> parent(){
+        
+    }
+    
+    /**
+    *This method sets the left subtree to the newLeft
+    *re-parents newLeft if not null
+    *@param newLeft the Binary subtree
+    */
+    public void setLeft(BinaryTree<E> newLeft){
+        
+    }
+    
+    /**
+    *This method sets the right subtree to the newRight
+    *re-parents newRight if not null
+    *@param newRight the Binary subtree
+    */
+    public void setRight(BinaryTree<E> newRight){
+        
+    }
+    
+    
+    /**
+    *This method re-parents this node to newParent reference or null
+    *@param newParent the new Parent of the tree; it may be null
+    */
+    protected void setParent(BinaryTree<E> newParent){
+        
+    }
+   
+    
+    /**
+    *
+    *@return  an in-order iterator of the elements
+    */
+    public Iterator<E> iterator(){
+        
+    }
+    
+    /**
+    *@return true if this is a left child of the parent
+    */
+    public boolean isLeftChild(){
+        
+    }
+    
+    
+    /**
+    *@return true if this is a right child of the parent
+    */
+    public boolean isRightChild(){
+        
+    }
+    
+    
+    /**
+    *@return the value associated with this node
+    */
+    public E value(){
+        
+    }
+    
+    
+    /**
+    *This method sets the vale associated with this node
+    *@param the value
+    */
+    public void setValue(E value){
+        
+    }
     
     
 }
